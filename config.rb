053c497cf84b12df392fb5f174b167ec2e3c4336
Jessica Scianimanica <sciani.jessica@gmail.com>
2017-08-07 15:04:16 +0100

    begin dynamic page

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |name, info|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name, info: info.city_slug }, ignore: true
+end
